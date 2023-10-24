f3f490dc8d6f15aca01a30e1386622d230fcd39c
Your name <you@example.com>
2023-10-25 00:37:10 +0900

    d

--- syspro/chap4/prob10/cat.c
+++ syspro/chap4/prob10/cat.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
-{
-   FILE *fp1, *fp2, *fp3;
-   int c;
-   if (argc < 4){ 
-      fp1 = stdin;
-      fp2 = stdin;
-      fp3 = stdin;
-   } 
-   else{  fp1 = fopen(argv[1],"r");
-	 fp2 = fopen(argv[2],"r");
-	 fp3 = fopen(argv[3],"r");
-   }
-   c = getc(fp1); 	
-   while (c != EOF) { 	
-      putc(c, stdout); 	 
-      c = getc(fp1); 
-   }
-   c= getc(fp2);
-   while (c != EOF){
-	   putc(c, stdout);
-	   c= getc(fp2);
-   }
-   c= getc(fp3);
-   while(c!= EOF){
-	   putc(c, stdout);
-	   c= getc(fp3);
-   }
-   fclose(fp1);
-   fclose(fp2);
-   fclose(fp3); 
-   return 0;
+#include <stdlib.h>
+#include <string.h>
+
+void copyFile(FILE *input, FILE *output, int addLineNumbers) {
+    int c;
+    int lineNumber = 1;
+    int newLine = 1;
+
+    while ((c = getc(input)) != EOF) {
+        if (addLineNumbers && newLine) {
+            fprintf(output, "%d ", lineNumber);
+            newLine = 0;
+        }
+        fputc(c, output);
+
+        if (c == '\n') {
+            lineNumber++;
+            newLine = 1;
+        }
+    }
+
+    if (!newLine) {
+        fputc('\n', output);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr,"test\n",argv[0]);
+   fprintf(stderr,"test\n",argv[0]);
+        return 1;
+    }
+
+    int addLineNumbers = 0;
+    int fileIndex = 1;
+
+    if (argc > 2 && strcmp(argv[1], "-n") == 0) {
+        addLineNumbers = 1;
+        fileIndex = 2;
+    }
+
+    for (int i = fileIndex; i < argc; i++) {
+        FILE *input = fopen(argv[i], "r");
+        if (input == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+
+        copyFile(input, stdout, addLineNumbers);
+
+        fclose(input);
+    }
+
+    return 0;
 }
